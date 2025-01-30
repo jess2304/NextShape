95f8cb21f86229ec65c82df0e2d0a22bc8dfbb86
Jessem Ettaghouti <85866619+jess2304@users.noreply.github.com>
2025-01-30 16:00:51 +0100

    Feat[SCRUM-1] : Install the Architecture of the Project + Layout  (#1)
    * feat[SCRUM-1] : Make a UI and WS with Vue and Django + Pre-commit params and gitignore
    
    * feat[SCRUM-1] : Install primevue and primeflex and make the layout of the website
    
    * feat[SCRUM-1] : add the icons

---
+++ UI/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import NavBarComponent from "./components/NavBarComponent.vue"
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-column">
+    <header class="z-3">
+      <NavBarComponent class="shadow-2 m-1" />
+    </header>
+    <main class="flex-grow-1 flex flex-column p-4"></main>
+    <footer class="z-3 shadow-2 text-center"></footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ UI/src/components/NavBarComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Menubar from "primevue/menubar"
+import Button from "primevue/button"
+</script>
+<template>
+  <Menubar :model="[]" class="p-0">
+    <template #start>
+      <div class="flex align-items-center">
+        <img src="/nextshape.png" alt="Next Shape Icon" class="w-1" />
+        <Button text label="Next Shape" class="text-4xl no-hover p-0" /></div
+    ></template>
+  </Menubar>
+</template>
+<style>
+.no-hover:hover {
+  background-color: inherit !important;
+}
+</style>
