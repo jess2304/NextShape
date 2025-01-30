194e1984eed6702144334cea41d37186f3c2921f
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-01-30 15:41:07 +0100

    feat[SCRUM-1] : add the icons

--- UI/src/components/NavBarComponent.vue
+++ UI/src/components/NavBarComponent.vue
@@ -5,8 +5,10 @@ import Button from "primevue/button"
 <template>
   <Menubar :model="[]" class="p-0">
     <template #start>
-      <Button text label="Next Shape" class="text-4xl no-hover" />
-    </template>
+      <div class="flex align-items-center">
+        <img src="/nextshape.png" alt="Next Shape Icon" class="w-1" />
+        <Button text label="Next Shape" class="text-4xl no-hover p-0" /></div
+    ></template>
   </Menubar>
 </template>
 <style>
