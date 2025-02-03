fe97b66464fbd1a34569cad2eb7249a4aa520ce8
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-02-03 14:57:49 +0100

    feat[SCRUM-2] : NavBar with style and responsive design

---
+++ UI/src/assets/js/constants.ts
@@ -0,0 +1,32 @@
+// constants.ts is a file for all the CONSTANTS we can use in the Frontend application
+
+export const NAVBAR_ELEMENTS = [
+  {
+    label: "Calculatrice",
+    icon: "pi pi-calculator",
+    items: [
+      {
+        label: "Calculatrice d'IMC",
+      },
+      {
+        label: "Calculatrice de calories",
+      },
+    ],
+  },
+  {
+    label: "Historique",
+    icon: "pi pi-history",
+  },
+  {
+    label: "Évolution",
+    icon: "pi pi-chart-line",
+  },
+  {
+    label: "Prédiction",
+    icon: "pi pi-chart-bar",
+  },
+  {
+    label: "Contact",
+    icon: "pi pi-envelope",
+  },
+]
