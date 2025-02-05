8b7767f3d00848c3ada2b5bb43fa178be8dd7782
Jessem Ettaghouti <85866619+jess2304@users.noreply.github.com>
2025-02-05 16:49:03 +0100

    feat[SCRUM-2] : NavBar with style and responsive design (#2)

--- UI/src/components/NavBarComponent.vue
+++ UI/src/components/NavBarComponent.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
 import Menubar from "primevue/menubar"
 import Button from "primevue/button"
+import { NAVBAR_ELEMENTS } from "@/assets/js/constants"
 </script>
 <template>
-  <Menubar :model="[]" class="p-0">
+  <Menubar :model="NAVBAR_ELEMENTS">
     <template #start>
-      <div class="flex align-items-center">
-        <img src="/nextshape.png" alt="Next Shape Icon" class="w-1" />
-        <Button text label="Next Shape" class="text-4xl no-hover p-0" /></div
-    ></template>
+      <img
+        src="/nextshape.png"
+        alt="Next Shape Icon"
+        class="w-1 flex-shrink-0" />
+      <Button text label="Next Shape" class="text-4xl no-hover p-0"
+    /></template>
+    <template #item="{ item, props, hasSubmenu }">
+      <router-link
+        v-if="item.route"
+        v-slot="{ href, navigate }"
+        :to="item.route"
+        custom
+      >
+        <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+          <span v-if="item.icon" :class="item.icon" class="text-xl" />
+          <span>{{ item.label }}</span>
+        </a>
+      </router-link>
+
+      <a
+        v-else
+        v-ripple
+        :href="item.url"
+        :target="item.target"
+        v-bind="props.action"
+      >
+        <span v-if="item.icon" :class="item.icon" class="text-xl" />
+        <span>{{ item.label }}</span>
+        <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+      </a>
+    </template>
   </Menubar>
 </template>
 <style>
--- UI/src/main.ts
+++ UI/src/main.ts
@@ -3,6 +3,8 @@ import "./style.css"
 import App from "./App.vue"
 import PrimeVue from "primevue/config"
 import Aura from "@primevue/themes/aura"
+import "primeicons/primeicons.css"
+import Ripple from "primevue/ripple"
 
 const app = createApp(App)
 app.use(PrimeVue, {
@@ -10,4 +12,5 @@ app.use(PrimeVue, {
     preset: Aura,
   },
 })
+app.directive("ripple", Ripple)
 app.mount("#app")
