4d4e2d39d3e1cfd32d5fd39161eea973189608c1
Jessem Ettaghouti <85866619+jess2304@users.noreply.github.com>
2025-02-11 11:01:50 +0100

    SCRUM-4 : Presentation Text + Buttons + Adjust the overflow for just main (#4)
    * SCRUM-4 : Make the presentation text
    
    * SCRUM-4 : Adjust the overflow on only main and give it a style (thin width black colour)
    
    * SCRUM-4 : Adjust space in html code

--- UI/src/assets/js/constants.ts
+++ UI/src/assets/js/constants.ts
@@ -36,3 +36,10 @@ export const NAVBAR_ELEMENTS = [
     route: "/contact",
   },
 ]
+
+// Presentation texts for home
+export const TITLE = "Transforme ton corps, contrôle tes calories !"
+export const SUBTITLE =
+  "NextShape t'aide à suivre ton évolution physique avec une précision inégalée."
+export const PRESENTATION_TEXT =
+  "Tu cherches un moyen simple et efficace de suivre ton évolution physique ? NextShape est là pour t'aider à calculer ton IMC, déterminer tes besoins caloriques et visualiser tes progrès au fil du temps. Que tu sois en phase de perte de poids, de prise de masse ou simplement curieux de voir comment ton corps évolue, cette plateforme te permet d'avoir une vue d'ensemble claire et détaillée de tes données. Pas d'algorithmes compliqués ni de recommandations approximatives : juste des calculs précis et des graphiques pour t'aider à mieux comprendre ton évolution et ajuster ton mode de vie en fonction de tes propres objectifs."
