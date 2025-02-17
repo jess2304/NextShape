f155c2c6934f84d8839d8d529b423b48675e85d6
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-02-17 10:55:52 +0100

    SCRUM-6 : Make the footer

---
+++ UI/src/components/FooterComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+</script>
+<template>
+  <div class="py-2">
+    <p class="m-0 text-600 text-sm">
+      © {{ currentYear }} NextShape - Tous droits réservés.
+    </p>
+  </div>
+</template>
+<style scoped></style>
