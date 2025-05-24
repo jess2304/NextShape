aa3fd50b145d9e2e037f45197c4c85f77d0dd83b
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-05-24 22:31:20 +0200

    SCRUM-11 : Make the entrypoint, the docker compose modifications  and the github actions workflow

--- WS/next_shape_ws/settings.py
+++ WS/next_shape_ws/settings.py
@@ -97,11 +97,11 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-DATABASE_USER = os.getenv("DATABASE_USER")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-DATABASE_HOST = os.getenv("DATABASE_HOST")
-DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
+DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
 
 DATABASES = {
     "default": {
---
+++ WS/wait.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+HOST_PORT=$1
+shift
+CMD="$@"
+
+HOST=$(echo $HOST_PORT | cut -d: -f1)
+PORT=$(echo $HOST_PORT | cut -d: -f2)
+
+echo "Waiting for DataBase for $HOST:$PORT..."
+
+while ! nc -z $HOST $PORT; do
+  sleep 0.5
+done
+
+echo "The DataBase is ready."
+exec $CMD
