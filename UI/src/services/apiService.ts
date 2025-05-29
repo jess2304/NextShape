cee53eab503044ee2b8ef21220867cf345f8d0d6
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-05-29 17:30:44 +0200

    SCRUM-11 : Clean the structure of docker-compose dev.yml and VITE_API_URL

--- UI/src/services/apiService.ts
+++ UI/src/services/apiService.ts
@@ -4,7 +4,7 @@ import { useAuthStore } from "@/stores/authStore"
 import { useToast } from "primevue/usetoast"
 import router from "@/router"
 
-const API_URL = "http://127.0.0.1:8000/api/"
+const API_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
   baseURL: API_URL,
