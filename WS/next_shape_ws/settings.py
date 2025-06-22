accb08bf572bf821a826f7d205853f7423fb4830
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-06-22 19:10:41 +0200

    FIX : recreate brevo.pem

--- WS/next_shape_ws/settings.py
+++ WS/next_shape_ws/settings.py
@@ -118,7 +118,7 @@ EMAIL_BACKEND = os.getenv(
     "EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend"
 )
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT") or 587)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
