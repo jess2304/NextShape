d35fabc39ace0d7bb49b7209bb3d1efb6722efb2
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-07-27 18:50:45 +0200

    Make docker cleaner

--- WS/entrypoint.sh
+++ WS/entrypoint.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
+set -e
 
 echo "ENV = $ENV"
 
 ./wait.sh "$DATABASE_HOST:$DATABASE_PORT"
 
-# Apply migrations
 echo "Apply the migrations"
 python manage.py migrate
 
-if [ "$ENV" = "dev" ]; then
-    echo "Development mode"
-    echo "Seeding the database..."
+case "$ENV" in
+  dev)
+    echo "Seeding database..."
     python manage.py seed || echo "Seeding failed or already done"
     exec python manage.py runserver 0.0.0.0:8000
-elif [ "$ENV" = "test" ]; then
-    echo "Testing mode"
+    ;;
+  test)
     exec pytest --disable-warnings --cov=api
-elif [ "$ENV" = "prod" ]; then
-    echo "Production mode"
+    ;;
+  prod)
     exec gunicorn next_shape_ws.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120
-else
-    echo "Unknown ENV : $ENV"
+    ;;
+  *)
+    echo "Unknown ENV: $ENV"
     exit 1
-fi
+    ;;
+esac
