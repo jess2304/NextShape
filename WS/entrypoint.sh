2c51ee1815a25d2a3b00b447cb259299e2798baa
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-07-27 19:16:15 +0200

    Reset to normal

--- WS/entrypoint.sh
+++ WS/entrypoint.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
-set -e
 
 echo "ENV = $ENV"
 
 ./wait.sh "$DATABASE_HOST:$DATABASE_PORT"
 
+# Apply migrations
 echo "Apply the migrations"
 python manage.py migrate
 
-case "$ENV" in
-  dev)
-    echo "Seeding database..."
+if [ "$ENV" = "dev" ]; then
+    echo "Development mode"
+    echo "Seeding the database..."
     python manage.py seed || echo "Seeding failed or already done"
     exec python manage.py runserver 0.0.0.0:8000
-    ;;
-  test)
+elif [ "$ENV" = "test" ]; then
+    echo "Testing mode"
     exec pytest --disable-warnings --cov=api
-    ;;
-  prod)
+elif [ "$ENV" = "prod" ]; then
+    echo "Production mode"
     exec gunicorn next_shape_ws.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120
-    ;;
-  *)
-    echo "Unknown ENV: $ENV"
+else
+    echo "Unknown ENV : $ENV"
     exit 1
-    ;;
-esac
+fi
