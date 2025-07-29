be8e213a1dc59a3490bebe10209997e685fd2480
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-07-29 21:43:45 +0200

    fix : add logs for debug

--- WS/api/authentication.py
+++ WS/api/authentication.py
@@ -1,4 +1,8 @@
+import logging
+
 from rest_framework_simplejwt.authentication import JWTAuthentication
+
+logger = logging.getLogger(__name__)
 
 
 class CookieJWTAuthentication(JWTAuthentication):
@@ -8,10 +12,17 @@ class CookieJWTAuthentication(JWTAuthentication):
 
     def authenticate(self, request):
         access_token = request.COOKIES.get("access_token")
+        logger.info(
+            f"[Auth] access_token found in cookie? {'Yes' if access_token else 'No'}"
+        )
         if access_token is None:
             return None
         try:
             validated_token = self.get_validated_token(access_token)
+            logger.info(
+                f"[Auth] Token valide pour user: {self.get_user(validated_token)}"
+            )
             return self.get_user(validated_token), validated_token
-        except Exception:
+        except Exception as e:
+            logger.warning(f"[Auth] Token invalide: {e}")
             return None
