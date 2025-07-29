f85d8fe35d32e9f441476300882f15531d2f2295
jess2304 <jessemtaghouti.prepa@gmail.com>
2025-07-29 21:50:11 +0200

    fix : add logs for debug

--- WS/next_shape_ws/settings.py
+++ WS/next_shape_ws/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -229,3 +230,32 @@ def get_cookie_settings():
 
 
 COOKIE_PARAMS = get_cookie_settings()
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "api": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
